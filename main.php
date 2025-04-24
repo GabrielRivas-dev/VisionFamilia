15ed61191fa53e8301b95850695cc0ed0bf279ef
Olidani619 <dolivaraldana@gmail.com>
2025-04-23 23:13:58 -0400

    Add files via upload

--- main.php
+++ main.php
@@ -19,12 +19,14 @@
     <h2>Visión de Familia</h2>
   </section>
   <section class="quienes-somos">
-    <h3>¿Quienes somos?</h3>
-    <p>Somos un Ministerio Apostólico y Profético, que nace del corazón de Dios,
-      fundamentados sobre la doctrina de los apóstoles, basados en la
-      predicación del evangelio, reconociendo al Padre al Hijo y al Espíritu Santo,
-      Bajo la dirección de los Pastores Juan Carlo Mata y Yenny de Mata, en
-      conjunto de un equipo de LÍderes.</p>
+    <div class="quienes-somos-content">
+        <h3>¿Quienes somos?</h3>
+        <p>Somos un Ministerio Apostólico y Profético, que nace del corazón de Dios,
+            fundamentados sobre la doctrina de los apóstoles, basados en la
+            predicación del evangelio, reconociendo al Padre al Hijo y al Espíritu Santo,
+            Bajo la dirección de los Pastores Juan Carlo Mata y Yenny de Mata, en
+            conjunto de un equipo de LÍderes.</p>
+    </div>
   </section>
   <section class="que-hacen">
     <div class="div1">
@@ -54,35 +56,29 @@
     <article>Adoración</article>
     <article>Escuela de la visión</article>
   </section>
-  <section class="objetivos">
-    <h3>Objetivos</h3>
-    <article>
-      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
-      <p>Que las almas lleguen al Reino de los Cielos.</p>
-    </article>
-    <article>
-      <p>Alcanzar y ganar las Almas perdidas.</p>
-      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
-    </article>
-    <article>
-      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
-      <p>Ayudarles a Perseverar en Dios.</p>
-    </article>
-  </section>
-  <section class="propositos">
-    <h3>Propósitos</h3>
-    <p>Guiar a las familias en el camino de crecimiento conjunto, amor, comprensión y desarrollo de sus potencialidades
-      humanas y espirituales.</p>
-  </section>
-  <section class="actividades">
-    <h3>Actividades que se realizan</h3>
-    <p>Talleres de comunicación, jornadas familiares, charlas motivacionales, asesorías individuales y acompañamiento en
-      procesos familiares.</p>
-  </section>
-  <section class="responsables">
-    <h3>Responsables</h3>
-    <p>El equipo está conformado por orientadores</p>
-  </section>
+  <div class="container">
+  <div class="section">
+    <h2>Objetivos</h2>
+    <p>Que las almas lleguen al Reino de los Cielos.</p>
+    <p>Alcanzar y ganar las Almas perdidas.</p>
+    <p>Ayudarles a Perseverar en Dios.</p>
+  </div>
+
+  <div class="section">
+    <h2>Propósitos</h2>
+    <p>Guiar a las familias en el camino de crecimiento conjunto, amor, comprensión y desarrollo de sus potencialidades humanas y espirituales.</p>
+  </div>
+
+  <div class="section">
+    <h2>Actividades que se realizan</h2>
+    <p>Talleres de comunicación, jornadas familiares, charlas motivacionales, asesorías individuales y acompañamiento en procesos familiares.</p>
+  </div>
+
+  <div class="section">
+    <h2>Responsables</h2>
+    <p>El equipo está conformado por orientadores.</p>
+  </div>
+</div>
   <h2 style="margin-left:20%; font-size:28px;">Fundadores</h2>
   <section class="fundadores">
     <div class="div1">
