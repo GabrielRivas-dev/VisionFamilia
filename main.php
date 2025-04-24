6cab70c5560118ab434eee7c4958c8f68cb51002
Gabriel Rivas <gaalex300@gmail.com>
2025-04-23 22:04:42 -0400

    primer commit

---
+++ main.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Visión de Familia</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+  <header>
+    <h1>Visión de Familia</h1>
+    <button onclick="window.scrollTo(0, 0)">Inicio</button>
+  </header>
+
+  <section class="portada">
+    <h2>Visión de Familia</h2>
+  </section>
+  <section class="quienes-somos">
+    <h3>¿Quienes somos?</h3>
+    <p>Somos un Ministerio Apostólico y Profético, que nace del corazón de Dios,
+      fundamentados sobre la doctrina de los apóstoles, basados en la
+      predicación del evangelio, reconociendo al Padre al Hijo y al Espíritu Santo,
+      Bajo la dirección de los Pastores Juan Carlo Mata y Yenny de Mata, en
+      conjunto de un equipo de LÍderes.</p>
+  </section>
+  <section class="que-hacen">
+    <div class="div1">
+      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
+    </div>
+    <div class="div2">
+      <h3>¿Que hacemos?</h3>
+      <article>
+        <p><strong>Servicios dominicales</strong><br> Encuentros semanales para adorar juntos y recibir enseñanzas</p>
+      </article>
+      <article>
+      <p><strong>Estudios biblicos</strong><br> Reuniones grupales para profundizar el conocimiento de la palabra</p>
+      </article>
+      <article>
+      <p><strong>Consejeria y apoyo</strong><br> Acompañamiento espiritual y emocional para las familias</p>
+      </article>
+    </div>
+  </section>
+  <h2 style="text-align: center; font-size:28px;">Departamentos</h2>
+  <section class="departamentos">
+    <article>Registro y seguimiento</article>
+    <article>Unidad de medios audiovisuales</article> 
+    <article>Protocolo</article> 
+    <article>Visionarios</article>
+    <article>VDG Kid's</article>
+    <article>Danza</article>
+    <article>Adoración</article>
+    <article>Escuela de la visión</article>
+  </section>
+  <section class="objetivos">
+    <h3>Objetivos</h3>
+    <article>
+      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
+      <p>Que las almas lleguen al Reino de los Cielos.</p>
+    </article>
+    <article>
+      <p>Alcanzar y ganar las Almas perdidas.</p>
+      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
+    </article>
+    <article>
+      <img src="https://images.pexels.com/photos/25204871/pexels-photo-25204871.jpeg" alt="">
+      <p>Ayudarles a Perseverar en Dios.</p>
+    </article>
+  </section>
+  <section class="propositos">
+    <h3>Propósitos</h3>
+    <p>Guiar a las familias en el camino de crecimiento conjunto, amor, comprensión y desarrollo de sus potencialidades
+      humanas y espirituales.</p>
+  </section>
+  <section class="actividades">
+    <h3>Actividades que se realizan</h3>
+    <p>Talleres de comunicación, jornadas familiares, charlas motivacionales, asesorías individuales y acompañamiento en
+      procesos familiares.</p>
+  </section>
+  <section class="responsables">
+    <h3>Responsables</h3>
+    <p>El equipo está conformado por orientadores</p>
+  </section>
+  <h2 style="margin-left:20%; font-size:28px;">Fundadores</h2>
+  <section class="fundadores">
+    <div class="div1">
+      <h3>Neptali y Katiusca De Peroza</h3>
+    </div>
+    <div class="div2">
+      <img src="https://iglesiafiladelfiajv.com/sites/default/files/abraham-y-betty.jpg" alt="">
+    </div>
+  </section>
+  <section class="contactos">
+    <h3>Contactos</h3>
+    <p> direccion: JQFF+5XH, Barinas 5201, Barinas, Venezuela</p>
+  </section>
+  <footer>
+    &copy; 2025 Visión de Familia. Todos los derechos reservados.
+  </footer>
+</body>
+
+</html>
